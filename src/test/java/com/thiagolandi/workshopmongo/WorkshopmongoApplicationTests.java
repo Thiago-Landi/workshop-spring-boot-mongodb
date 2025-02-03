c034cdd55863eb8ac9b6ef5a7e5fbb3443bd50c4
Thiago-Landi <thiago0902@gmail.com>
2025-02-03 10:34:56 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/thiagolandi/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagolandi.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
