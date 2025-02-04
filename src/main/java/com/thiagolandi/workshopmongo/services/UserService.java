133cf9aa5ce474e426f4041cc5af8ed72620b872
Thiago-Landi <thiago0902@gmail.com>
2025-02-04 08:41:47 -0300

    Insercao de User com post

--- src/main/java/com/thiagolandi/workshopmongo/services/UserService.java
+++ src/main/java/com/thiagolandi/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.thiagolandi.workshopmongo.domain.User;
+import com.thiagolandi.workshopmongo.dto.UserDTO;
 import com.thiagolandi.workshopmongo.repositoy.UserRepository;
 import com.thiagolandi.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,15 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));// vai lançar o obj e caso esteja vazia, vai lançar uma excessão
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		User user = new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+		return user;
 	}
 }
 
