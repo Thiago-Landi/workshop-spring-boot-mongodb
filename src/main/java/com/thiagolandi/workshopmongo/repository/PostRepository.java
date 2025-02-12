243399983d6b106367f89db9ae2a1c157a9cdcba
Thiago-Landi <thiago0902@gmail.com>
2025-02-12 08:07:02 -0300

    Criação do metodo fullSearch

--- src/main/java/com/thiagolandi/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/thiagolandi/workshopmongo/repository/PostRepository.java
@@ -1,8 +1,10 @@
 package com.thiagolandi.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.thiagolandi.workshopmongo.domain.Post;
@@ -12,4 +14,8 @@ public interface PostRepository extends MongoRepository<Post, String>{
 
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	// esses sao comandos especificos do mongoDB
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: {$lte: ?2} }, { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
